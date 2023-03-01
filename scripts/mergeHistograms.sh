f242d8470bf28bf91c739b4488799c6f4b387c0e
Aidan Grummer <agrummer@fnal.gov>
2023-03-01 14:58:06 -0600

    Apply all trig cut BDTs (nominal,sans mX,mY,dfd); Fill for Variable plots

--- scripts/mergeHistograms.sh
+++ scripts/mergeHistograms.sh
@@ -3,7 +3,8 @@
 # tag=2023Feb22_Mxlt800 # check the mx and my relationship by cutting on mx at 800 and making two sets of plots
 # tag=2023Feb22_Mygt140lt190 # check the mx and my relationship by cutting on mx at 800 and making two sets of plots
 # tag=2023Feb27_TrigCut_5 # check how many events pass cut at trig threshold
-tag=2023Feb28_3 # Full Submission with pt and ht cuts that match the HLT trigger thresholds, the _3 is to include the Higgs variables in the output (I should not have removed them)
+# tag=2023Feb28_3 # Full Submission with pt and ht cuts that match the HLT trigger thresholds, the _3 is to include the Higgs variables in the output (I should not have removed them)
+tag=$1 # Full Submission with pt and ht cuts that match the HLT trigger thresholds, the _3 is to include the Higgs variables in the output (I should not have removed them)
 python ./scripts/mergeHistograms.py --tag fullSubmission_2016_BDTweights_${tag}
 python ./scripts/mergeHistograms.py --tag fullSubmission_2017_BDTweights_${tag}
 python ./scripts/mergeHistograms.py --tag fullSubmission_2018_BDTweights_${tag}
--- scripts/submitAllFillOnTier3_RunII.sh
+++ scripts/submitAllFillOnTier3_RunII.sh
@@ -1,4 +1,10 @@
+# # submit a full fill with pt and ht cuts that match the HLT trigger thresholds
+# python scripts/submitFillOnTier3.py --tag fullSubmission_2016_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2016Resonant_NMSSM_XYH_bbbb_Full.cfg
+# python scripts/submitFillOnTier3.py --tag fullSubmission_2017_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2017Resonant_NMSSM_XYH_bbbb_Full.cfg
+# python scripts/submitFillOnTier3.py --tag fullSubmission_2018_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2018Resonant_NMSSM_XYH_bbbb_Full.cfg
+
 # submit a full fill with pt and ht cuts that match the HLT trigger thresholds
-python scripts/submitFillOnTier3.py --tag fullSubmission_2016_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2016Resonant_NMSSM_XYH_bbbb_Full.cfg
-python scripts/submitFillOnTier3.py --tag fullSubmission_2017_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2017Resonant_NMSSM_XYH_bbbb_Full.cfg
-python scripts/submitFillOnTier3.py --tag fullSubmission_2018_BDTweights_2023Feb28_3 --cfg config/Resonant_NMSSM_bbbb/plotter_2018Resonant_NMSSM_XYH_bbbb_Full.cfg
+# plot Vars only
+python scripts/submitFillOnTier3.py --tag fullSubmission_2016_BDTweights_2023Feb28_vars --cfg config/Resonant_NMSSM_bbbb/plotter_2016Resonant_NMSSM_XYH_bbbb_Full.cfg
+python scripts/submitFillOnTier3.py --tag fullSubmission_2017_BDTweights_2023Feb28_vars --cfg config/Resonant_NMSSM_bbbb/plotter_2017Resonant_NMSSM_XYH_bbbb_Full.cfg
+python scripts/submitFillOnTier3.py --tag fullSubmission_2018_BDTweights_2023Feb28_vars --cfg config/Resonant_NMSSM_bbbb/plotter_2018Resonant_NMSSM_XYH_bbbb_Full.cfg
