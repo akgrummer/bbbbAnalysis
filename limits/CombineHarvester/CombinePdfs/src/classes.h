b7bfee12ccf1ce35b8201be509ae316b632eb6e8
Aidan Grummer <akgrummer@gmail.com>
2024-10-15 17:09:49 -0500

    remove CombineHarvester files: only used for testing not in final analysis

--- limits/CombineHarvester/CombinePdfs/src/classes.h
+++
@@ -1,3 +0,0 @@
-#include "CombineHarvester/CombinePdfs/interface/MorphFunctions.h"
-#include "CombineHarvester/CombinePdfs/interface/CMSHistFuncFactory.h"
-
--- limits/CombineHarvester/CombineTools/scripts/do_nothing_cfg.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-import FWCore.ParameterSet.Config as cms
-process = cms.Process("MAIN")
-
-process.source = cms.Source("EmptySource")
-
